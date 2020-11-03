f4f0ffeef62b8b946b9f95d07dc35def6d0f7354
santiagonoguez <santiago.arriaga9@gmail.com>
2020-11-03 09:09:14 -0600

    add user status constants

---
+++ src/main/java/com/theheroloop/events/UserStatus.java
@@ -0,0 +1,9 @@
+package com.theheroloop.events;
+
+public interface UserStatus
+{
+  int USER_STATUS_DISABLED               = -1;
+  int USER_STATUS_NOT_AVAILABLE          =  0;
+  int USER_STATUS_AVAILABLE              =  1;
+  int USER_STATUS_SOON_WILL_BE_AVAILABLE =  2; // still not implemented
+}
